a0ee76b2bd65346c2c2afe1b48548e984e85a81d
sarunas0123 <sarunasxx7@gmail.com>
2021-02-03 11:28:56 +0200

    penktas aasdaasda

--- 5 ex.py
+++ 5 ex.py
@@ -1,11 +1,13 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13]
 
-lenght = max(len(a), len(b))
+lenght = min(len(a), len(b))
 c = []
-
-for i in lenght:
-    if a[i] in b[i]:
-        c.append(a[i])
+i=0
+while i<lenght:
+    if a[i] in b:
+        if a[i] not in c:
+            c.append(a[i])
+    i += 1
 
 print(c)
